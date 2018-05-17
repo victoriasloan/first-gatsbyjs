4c91656042349b9d38e3a1065bb89e816da85368
Victoria Sloan <victoriasloan@victorias-mbp.home>
2018-05-17 21:41:40 +0100

    basic site with navigation and layouts

---
+++ tutorial-part-three/src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <h1>About me</h1>
+    <p>23 | JavaScript Developer | Belfast</p>
+    <p>Graduated from Ulster University, Jordanstown campus</p>
+  </div>
+);
---
+++ tutorial-part-three/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <h1>Welcome</h1>
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar odio sagittis iaculis vehicula. Morbi at sapien quis sem viverra pretium. Integer pretium eros nunc, vehicula maximus ante volutpat at. In ultricies malesuada odio hendrerit dictum. Duis mattis tellus dolor, vel semper justo pretium non. Donec massa eros, vestibulum at augue maximus, dignissim condimentum tortor. Aenean hendrerit, tortor quis feugiat pretium, quam ligula feugiat urna, at porttitor mi diam non sapien. Fusce accumsan urna ultricies orci ultrices interdum. Aliquam et arcu quis tellus maximus hendrerit. 
+    </p>
+  </div>
+);
